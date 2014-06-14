5b5fb6650cc6a8270bb1c46bcb39ec46aeb25361
sangche <asopes@gmail.com>
2014-06-13 17:38:15 -0700

    organize - init2

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ app.config(function ($routeProvider) {
                 templateUrl: '/partials/home.html'
             })
         //Define a route that has a route parameter in it (:customerID)
-        .when('/articles/:articleID',
+        .when('/people/:personID',
             {
-                controller: 'articlesController',
-                templateUrl: '/partials/articles.html'
+                controller: 'peopleController',
+                templateUrl: '/partials/people.html'
+            })
+        .when('/people',
+            {
+                controller: 'peopleAllController',
+                templateUrl: '/partials/people.html'
             })
         .when('/about',
             {
@@ -20,4 +25,46 @@ app.config(function ($routeProvider) {
                 templateUrl: '/partials/about.html'
             })
         .otherwise({ redirectTo: '/' });
+
 });
+
+app.service('myService', function() {
+    this.getPeople = function() {
+        return people;
+    };
+
+    this.getPerson = function(id) {
+        return people[id];
+    };
+
+    var people = [
+        {name: "John", city: "New York"},
+        {name: "Tom", city: "Atlanta"},
+        {name: "Kelly", city: "Seattle"},
+        {name: "Ann", city: "Phoenix"}
+        ];   
+});
+
+app.controller('homeController', function($scope) {
+    $scope.message = "welcome to home page!";
+});
+
+app.controller('peopleController', function($scope, $routeParams, myService) {
+    $scope.message = "welcome to people page!";
+    var pID = ($routeParams.personID) ? parseInt($routeParams.personID) : -1;
+    if (pID < 0) {
+        $scope.people = myService.getPeople();
+    } else {
+        $scope.people = [myService.getPerson(pID)];
+    }
+});
+
+app.controller('peopleAllController', function($scope, myService) {
+    $scope.message = "welcome to people page!";
+    $scope.people = myService.getPeople();
+});
+
+app.controller('aboutController', function($scope) {
+    $scope.message = "welcome to about page!";
+});
+
